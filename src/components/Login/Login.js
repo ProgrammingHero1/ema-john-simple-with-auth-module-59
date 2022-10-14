f39a0e60a2d69a612995896ab1b63161d9d49851
Programming Hero <programming.hero1@gmail.com>
2022-10-14 15:41:11 -0500

    login style

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>New to ema john <Link to='/signup'>Create a New Account</Link></p>
         </div>
     );
 };
